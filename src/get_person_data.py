122cd3bb0e845ad21274761557e8c62983593047
MateiElena <mateielena214@yahoo.com>
2024-11-20 21:56:09 +0100

    modified get_person_data

--- src/get_person_data.py
+++ src/get_person_data.py
@@ -16,17 +16,18 @@ DATA = {
 def get_name_and_age(person_id):
     return DATA[person_id]
 
+
 def save_data(file, person_data):
     """Save person data to file"""
     with open(file, "w") as fw:
         fw.write(person_data)
+
 
 def read_data(file):
     """Return a list with all rows from file"""
     with open(file, "r") as fr:
         content = fr.readlines()    #returneaza o lista
     return content
-
 
 if __name__ == "__main__":
     try:
